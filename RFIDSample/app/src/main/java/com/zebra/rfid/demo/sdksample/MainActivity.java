5cdfae226edda92da5e9c848f39fb0de6276fe37
Mohib Rehman <mohib.rehman@proact.net>
2023-03-06 11:56:47 +0000

    completed integration in fresh project

---
+++ RFIDSample/app/src/main/java/com/zebra/rfid/demo/sdksample/MainActivity.java
@@ -0,0 +1,142 @@
+package com.zebra.rfid.demo.sdksample;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.zebra.rfid.api3.TagData;
+
+public class MainActivity extends AppCompatActivity implements RFIDHandler.ResponseHandlerInterface {
+
+    public TextView statusTextViewRFID = null;
+    private TextView textrfid;
+    private TextView testStatus;
+
+    RFIDHandler rfidHandler;
+    final static String TAG = "RFID_SAMPLE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        // UI
+        statusTextViewRFID = findViewById(R.id.textStatus);
+        textrfid = findViewById(R.id.textViewdata);
+        testStatus = findViewById(R.id.testStatus);
+
+        // RFID Handler
+        rfidHandler = new RFIDHandler();
+        rfidHandler.onCreate(this);
+
+        // set up button click listener
+        Button test = findViewById(R.id.button);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = rfidHandler.Test1();
+                testStatus.setText(result);
+            }
+        });
+
+        Button test2 = findViewById(R.id.button2);
+        test2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = rfidHandler.Test2();
+                testStatus.setText(result);
+            }
+        });
+
+        Button defaultButton = findViewById(R.id.button3);
+        defaultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = rfidHandler.Defaults();
+                testStatus.setText(result);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        rfidHandler.onPause();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        String status = rfidHandler.onResume();
+        statusTextViewRFID.setText(status);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        rfidHandler.onDestroy();
+    }
+
+
+    @Override
+    public void handleTagdata(TagData[] tagData) {
+        final StringBuilder sb = new StringBuilder();
+        for (TagData tagDatum : tagData) {
+            sb.append(tagDatum.getTagID()).append("\n");
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textrfid.append(sb.toString());
+            }
+        });
+    }
+
+    @Override
+    public void handleTriggerPress(boolean pressed) {
+        if (pressed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textrfid.setText("");
+                }
+            });
+            rfidHandler.performInventory();
+        } else
+            rfidHandler.stopInventory();
+    }
+}
